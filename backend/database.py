fce10371a3248945be6e67a7e72a3539be5fc0f7
Shirobaka <kegy5@live.de>
2026-05-16 02:47:19 +0200

    requirements.txt updated

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 from sqlalchemy import create_engine, Column, Integer, String, DateTime, Text, JSON, DECIMAL, Enum, ForeignKey, Boolean
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.sql import func
 from dotenv import load_dotenv
